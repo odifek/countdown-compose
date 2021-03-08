b237fb4e1cb5586a094279a3864be6e7d39f8e2a
odifek <kennedy998@gmail.com>
2021-03-08 23:16:21 +0100

    hoist selection update

--- app/src/main/java/com/example/countdownchallenge/Countdown.kt
+++ app/src/main/java/com/example/countdownchallenge/Countdown.kt
@@ -1,5 +1,6 @@
 package com.example.countdownchallenge
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.gestures.rememberDraggableState
@@ -17,6 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
 import java.util.*
 import kotlin.math.abs
+import kotlin.math.roundToInt
 
 data class Time(val minutes: Int, val seconds: Int) {
     operator fun dec(): Time {
@@ -35,8 +37,8 @@ data class Time(val minutes: Int, val seconds: Int) {
 fun CountDownScreen() {
 
     val initialTime = Time(1, 45)
-    var time by remember { mutableStateOf(initialTime)}
-    
+    var time by remember { mutableStateOf(initialTime) }
+
 //    LaunchedEffect(key1 = time.zero()) {
 //        while (true) {
 //            delay(1000)
@@ -45,66 +47,90 @@ fun CountDownScreen() {
 //    }
     CountDown(time = time)
 }
+
 @Composable
 fun CountDown(time: Time) {
+    var changeTime by remember { mutableStateOf(time) }
     Row(
         modifier = Modifier.fillMaxSize(),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        NumberWheel(range = (0..5), selected = time.minutes / 10)
-        NumberWheel(range = (0..9), selected = time.minutes % 10)
-        NumberWheel(range = (0..5), selected = time.seconds / 10)
-        NumberWheel(range = (0..9), selected = time.seconds % 10)
+        NumberWheel(range = (0..5), selected = changeTime.minutes / 10) {
+            changeTime = changeTime.copy(minutes = it * 10 + changeTime.minutes % 10)
+        }
+        NumberWheel(range = (0..9), selected = changeTime.minutes % 10) {
+            changeTime = changeTime.copy(minutes = changeTime.minutes / 10 * 10 + it)
+        }
+        NumberWheel(range = (0..5), selected = changeTime.seconds / 10) {
+            changeTime = changeTime.copy(seconds = it * 10 + changeTime.seconds % 10)
+        }
+        NumberWheel(range = (0..9), selected = changeTime.seconds % 10) {
+            changeTime = changeTime.copy(seconds = changeTime.seconds / 10 * 10 + it) // Replace the unit
+        }
     }
 }
 
 @Composable
-private fun NumberWheel(range: IntRange, selected: Int) {
+private fun NumberWheel(range: IntRange, selected: Int, onSelectionUpdate: (Int) -> Unit) {
 
-    val boxSize = 40.dp
-    var selection by remember { mutableStateOf(selected)}
+    val boxSizeDp = 40.dp
+    val boxSize = with(LocalDensity.current) { boxSizeDp.toPx() }
     val mid = (range.last - range.first) / 2f
-    val offset by remember { mutableStateOf(boxSize * (mid - selection))}
+    val offset = boxSize * (mid - selected)
+    var totalOffset by remember { mutableStateOf(offset) }
 
     var isMax = false
     var isMin = false
-    var offsetY by remember { mutableStateOf(0f)}
+    var offsetY by remember { mutableStateOf(0f) }
     val dragState = rememberDraggableState(onDelta = { delta ->
         // We don't want to record the drag if we've reached the end of the bar
+        var updateSelection = false
         val newOffset = offsetY + delta
         if (isMax) {
-            if (newOffset <= offsetY) offsetY = newOffset
+            if (newOffset <= offsetY) {
+                updateSelection = true
+                offsetY = newOffset
+            }
         } else if (isMin) {
-            if (newOffset >= offsetY) offsetY = newOffset
+            if (newOffset >= offsetY) {
+                updateSelection = true
+                offsetY = newOffset
+            }
         } else {
+            updateSelection = true
             offsetY = newOffset
+        }
+
+        val effectiveDragOffset =
+            boxSize * (offsetY / boxSize).toInt() // Round off to a number a multiple of boxSize
+
+        val maxOffset = abs(boxSize * mid)
+        totalOffset = (effectiveDragOffset + offset).coerceIn(-maxOffset, maxOffset)
+        isMin = totalOffset <= -maxOffset // Have we reached the minimum point
+        isMax = totalOffset >= maxOffset
+
+
+
+        if (updateSelection) {
+            val selection =
+                (mid - totalOffset / boxSize).toInt() // Calculate the current selection based on the total offset
+            onSelectionUpdate(selection)
         }
     })
 
-    val effectiveDragOffset = with(LocalDensity.current) {
-        boxSize * (offsetY.toDp() / boxSize).toInt() // Round off to a number a multiple of boxSize
-    }
-
-    val maxOffset = abs(boxSize.value * mid)
-    val totalOffset = (effectiveDragOffset + offset).coerceIn(-maxOffset.dp, maxOffset.dp)
-    isMin = totalOffset.value <= -maxOffset // Have we reached the minimum point
-    isMax = totalOffset.value >= maxOffset
-
-    selection = (mid - totalOffset / boxSize).toInt() // Calculate the current selection based on the total offset
-    // TODO: onSelectionUpdate(selection)
     Column(
         modifier = Modifier
-            .offset(y = totalOffset)
+            .offset { IntOffset(x = 0, y = totalOffset.roundToInt()) }
             .clip(RoundedCornerShape(25))
             .draggable(dragState, orientation = Orientation.Vertical)
     ) {
         range.forEach { n ->
             val background =
-                if (n == selection) MaterialTheme.colors.secondary else MaterialTheme.colors.primary
+                if (n == selected) MaterialTheme.colors.secondary else MaterialTheme.colors.primary
             Box(
                 modifier = Modifier
-                    .size(boxSize)
+                    .size(boxSizeDp)
                     .background(background),
                 contentAlignment = Alignment.Center
             ) {
